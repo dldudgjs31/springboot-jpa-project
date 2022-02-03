db57449c0aa9a111a587ddc57a69877e2584ecb1
dldudgjs31 <dldudgjs31@naver.com>
2022-02-03 23:51:42 +0900

    댓글 기능 구현

--- src/main/java/com/young/blog/model/Board.java
+++ src/main/java/com/young/blog/model/Board.java
@@ -53,7 +53,7 @@ public class Board {
 	// db에 컬럼 만들지마라는 의미(fk 가 아니라는 의미) // mapped by의 이름은 해당 엔티티의 변수명을 기입
 	// one to many는 기본이 lazy 전략이지만 현재 프로젝트에는 댓글도 무조건 노출하기 때문에 eager 전략으로 변경함
 	@JsonIgnoreProperties({"board"}) // 해당 설정시 reply 내부의 board를 연관참조하는 것을 방지 할수 있다.
-	@OrderBy("id desc") // 출력시 출력되는 순서 정렬
+	@OrderBy("id asc") // 출력시 출력되는 순서 정렬
 	private List<Reply> reply;
 	/*
 	 * ManyToONE : 기본적인 fetch (가져오는)전략 : FetchType.EAGER => 하나의 정보만 가져오면 되기때문 : 무조건 들고옴
